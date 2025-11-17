ad7c08deef31bbae3a928f4300754c3e0afadd50
Asia <asiaidrees283@gmail.com>
2025-11-17 09:59:02 -0500

    tip 001

--- index.js
+++ index.js
@@ -5,8 +5,48 @@
  */
 
 class Task {
- //write your code here
- 
+    // the private fields
+    #completed = false;
+    #minutesSpent = 0;
+
+    static AllTasks = [];
+
+    constructor(title, priority) {
+        this.title = title;
+        this.priority = priority;
+        Tasks.AllTasks.push(this);
+    }
+
+    get completed() {
+        return this.#completed;
+    }
+
+    get minutesSpent() {
+        return this.#minutesSpent;
+    }
+
+    workOn(minutes) {
+        this.#minutesSpent += minutes;
+        console.log(`Worked on ${this.title} for ${minutes} minutes. Total time: ${this.#minutesSpent} minutes.`);
+
+    }
+
+    complete() {
+        this.#completed = true;
+        console.log(`${this.title} has been completed!`);
+    }
+
+    isCompleted() {
+        return this.#completed;
+    }
+
+    static getTotalTasks() {
+        return Task.AllTasks.length;
+    }
+
+    static findByTitle() {
+        return Task.AllTasks.find(task => task.title === title);
+    };
 }
 
 //Test class with examples below:
